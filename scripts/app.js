6891bde3f6cfb9fd8324a5d758393ef44224a0b4
eleftheros <eleftheros_all@protonmail.com>
2021-05-21 17:22:17 +0300

    Add setMovieDetails function

--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,92 @@
 const searchForm = document.getElementById("searchForm");
 const movieList = document.getElementById("movieList");
 const apiKey = "d618021a";
-const base = `http://www.omdbapi.com/?apikey=${apiKey}&`;
+const base = `http://www.omdbapi.com/?apikey=${apiKey}&plot=full&`;
+
+//function to close details
+const closeDetails = (el) => {
+  el.innerHTML = "See More...";
+};
+
+//function to update movie details
+const setMovieDetails = (element, data) => {
+  console.log(element, data.imdbID);
+  let html = `
+    <span class="results__list__item__details__heading">Genre</span>
+    <span class="results__list__item__genre">${data.Genre}</span>
+    <span class="results__list__item__details__heading">Director</span>
+    <span class="results__list__item__director">${data.Director}</span>
+    <span class="results__list__item__details__heading">Actors</span>
+    <span class="results__list__item__actors">${data.Actors}</span>
+    <span class="results__list__item__details__heading">Imdb Rating</span>
+    <span class="results__list__item__rating">${data.Ratings[0].Value}</span>
+    <span class="results__list__item__details__heading">Duration</span>
+    <span class="results__list__item__duration">${data.Runtime}</span>
+    <span class="results__list__item__plot">${data.Plot}</span>
+  `;
+  
+  //create element to close details
+  const closeDetailsElement = document.createElement("div");
+  closeDetailsElement.classList.add("results__list__item__close");
+  closeDetailsElement.textContent = "Show Less...";
+  closeDetailsElement.addEventListener("click", () => {
+    closeDetails(element);
+  });
+
+  element.innerHTML = html;
+  element.appendChild(closeDetailsElement);
+};
+
+//function to get movie details
+const getMovieDetails = async id => {
+  const query = "i=";
+  const element = document.querySelector(`[imdb-id=${id}]`);
+
+  //fetch data
+  const response = await fetch(base + query + id);
+  const data = await response.json();
+
+  //get data
+  setMovieDetails(element, data);
+};
 
 //function to update Movie list
 const setMovieResult = result => {
-  console.log(result)
   //create elements
   const liElement = document.createElement("li");
   const titleElement = document.createElement("span");
   const yearElement = document.createElement("span");
   const imgElement = new Image();
+  const detailsElement = document.createElement("div");
+
+  //save id
+  const id = result.imdbID;
 
   //give classes and attributes
-  liElement.setAttribute("imdb-id", result.imdbID);
   liElement.classList.add("results__list__item");
   titleElement.classList.add("results__list__item__title");
   yearElement.classList.add("results__list__item__year");
   imgElement.classList.add("results__list__item__image");
+  detailsElement.classList.add("results__list__item__details");
+  detailsElement.setAttribute("imdb-id", id);
   
   //give content
   titleElement.textContent = result.Title;
   yearElement.textContent = result.Year;
   imgElement.src = result.Poster;
+  detailsElement.textContent = "See More...";
   
   //append elements
   liElement.appendChild(titleElement);
   liElement.appendChild(yearElement);
   
+  //add event listener to detailsElement
+  detailsElement.addEventListener("click", () => getMovieDetails(id));
   
   //set img async
   imgElement.onload = function() {
     liElement.appendChild(imgElement);
+    liElement.appendChild(detailsElement);
   }
   
   //append to the DOM
@@ -40,14 +95,14 @@ const setMovieResult = result => {
 
 //function to search movies
 const searchMovies = async(e) => {
-  //clear previous result
+  //clear previous results
   movieList.innerHTML = "";
 
   e.preventDefault();
-  const query = "plot=full&type=movie&s=";
+  const query = "type=movie&s=";
   const searchValue = e.target.search.value;
 
-  const response = await fetch(base + query + searchValue + "&page=3");
+  const response = await fetch(base + query + searchValue);
   const data = await response.json();
 
   //reset
