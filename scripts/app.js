022a6a46485a0c2dd79a8e4d0c0fa5a5ad735296
eleftheros <eleftheros_all@protonmail.com>
2021-05-21 13:23:46 +0300

    Add setMovieResult function

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,35 @@
 const searchForm = document.getElementById("searchForm");
+const movieList = document.getElementById("movieList");
 const apiKey = "d618021a";
 const base = `http://www.omdbapi.com/?apikey=${apiKey}&`;
 
 //function to update Movie list
 const setMovieResult = result => {
-  console.log(result);
+  //create elements
+  const liElement = document.createElement("li");
+  const titleElement = document.createElement("span");
+  const yearElement = document.createElement("span");
+  const imgElement = document.createElement("image");
+
+  //give classes and attributes
+  liElement.setAttribute("imdb-id", result.imdbID);
+  liElement.classList.add("results__list__item");
+  titleElement.classList.add("results__list__item__title");
+  yearElement.classList.add("results__list__item__year");
+  imgElement.classList.add("results__list__item__image");
+  imgElement.src = result.Poster;
+
+  //give content
+  titleElement.textContent = result.title;
+  yearElement.textContent = result.year;
+
+  //append elements
+  liElement.appendChild(titleElement);
+  liElement.appendChild(yearElement);
+  liElement.appendChild(imgElement);
+
+  //append to the DOM
+  movieList.appendChild(liElement);
 };
 
 //function to search movies
@@ -20,8 +45,7 @@ const searchMovies = async(e) => {
   e.target.reset();
 
   //get the data to the function
-  // data.Search.forEach(result => setMovieResult(result));
-  setMovieResult(data.Search[0]);
+  data.Search.forEach(result => setMovieResult(result));
 };
 
 //events
